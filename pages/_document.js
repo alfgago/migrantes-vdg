b04c8e80de24b6482dfcdfa0fdbeb6649dcbf327
alfgago <alfredo@5e.cr>
2022-03-28 11:57:30 -0600

    Add iframe resizer

--- pages/_document.js
+++ pages/_document.js
@@ -23,6 +23,7 @@ export default class MyDocument extends Document {
                     <link rel="stylesheet" type="text/css" charSet="UTF-8" href="https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.6.0/slick.min.css" /> 
                     <link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.6.0/slick-theme.min.css" />
                     <script src="/scripts/init-analytics.js"></script>
+                    <script src="https://cdnjs.cloudflare.com/ajax/libs/iframe-resizer/4.3.2/iframeResizer.contentWindow.js"></script>
                 </Head>
                 <body>
                     <div id="page-wrapper">
