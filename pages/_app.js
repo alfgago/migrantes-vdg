c3c89cb725c88d2682c9a60db696168d9c894bae
alfgago <alfredo@5e.cr>
2022-04-04 13:49:46 -0600

    Fix SEO

--- pages/_app.js
+++ pages/_app.js
@@ -7,17 +7,14 @@ import { useRouter } from 'next/router';
 import Layout from '@/components/Layout';
 
 function MyApp({ Component, pageProps }) {
-  const router = useRouter(); 
 
   return (
     <>
-      {router.isReady && router.pathname ? (
-          <PageTransition timeout={1000} classNames="page-transition">
-            <Layout>
-              <Component {...pageProps} />
-            </Layout>
-          </PageTransition>
-      ) : ''}
+      <PageTransition timeout={1000} classNames="page-transition">
+        <Layout>
+          <Component {...pageProps} />
+        </Layout>
+      </PageTransition>
     </>
   )
 }
